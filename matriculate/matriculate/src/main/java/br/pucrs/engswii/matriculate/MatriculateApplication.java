5747d281e5804fc6afa594494e223bc09ece7991
Giulio Dille Girardi <Giulio.Girardi@edu.pucrs.br>
2023-06-14 20:02:32 -0300

    feat: initial commit

---
+++ matriculate/matriculate/src/main/java/br/pucrs/engswii/matriculate/MatriculateApplication.java
@@ -0,0 +1,13 @@
+package br.pucrs.engswii.matriculate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculateApplication.class, args);
+	}
+
+}
---
+++ matriculate/matriculate/src/test/java/br/pucrs/engswii/matriculate/MatriculateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucrs.engswii.matriculate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
